339daf9b3b6af08a6af5a993a440561b26cd0f56
Yubikiri <onevoltten@googlemail.com>
2016-09-07 11:47:18 +1000

    sort.sh significant changes
    Revised layout
    Added comments
    Added subdirectory folder scanning
    bot.sh execute in seporate instance

--- sort.sh
+++ sort.sh
@@ -1,63 +1,46 @@
 #!/bin/bash
-cd /var/www/sort
-SORT="/var/www/sort"
-DOWNLOAD="/var/www/downloads"
-TRASH="/var/www/trash"
+cd /var/www/sort; SORT="/var/www/sort"; DOWNLOAD="/var/www/downloads"; TRASH="/var/www/trash"
 #TR_TORRENT_DIR="/var/www/sort"
 #TR_TORRENT_NAME="Tekkon_Kinkreet_(2006)_[720p,BluRay,x264]_-_THORA.mkv"
-
+TR_DOWNLOADS="/var/www/sort/$TR_TORRENT_NAME"; echo "TR_DOWNLOADS > $TR_DOWNLOADS" >> $SORT/log.txt
+# Function die with message
 die() { echo "$@" 1>&2 ; exit 1; }
-
-TR_DOWNLOADS="/var/www/sort/$TR_TORRENT_NAME"
-echo "TR_DOWNLOADS > $TR_DOWNLOADS" >> $SORT/log.txt
-
-echo 'retrieve...' >> $SORT/log.txt
 # Update downloading.txt if any changes
-/root/retrieve.sh; sleep 1
+echo 'retrieve...' >> $SORT/log.txt; /root/retrieve.sh; sleep 1
+# Foreach line in downloading.txt
 echo 'read downloading.txt' >> $SORT/log.txt
 while read line; do
-	FILEN=${line%:*:*:*:*:*}
-	Basename="${FILEN##*/}"
-	echo $line >> $SORT/log.txt
-	MALID=${line#*:*:*:*:*:}
-	VALUES=${line#*:}
-	TITLE=${VALUES%:*:*:*:*}			
-	FANSUB1=${VALUES%:*:*:*}
-	FANSUB=${FANSUB1#*:}
-	NEXT1=${VALUES%:*}
-	NEXT=${NEXT1#*:*:}
-	AUDIO=${NEXT%:*}
-	SUB=${NEXT#*:}
-	FTITLE="$MALID|$TITLE|$FANSUB|$AUDIO|$SUB"
-	echo Filetitle $FTITLE >> $SORT/log.txt
-
+	# Get meta between colon
+	FILEN=${line%:*:*:*:*:*}; Basename="${FILEN##*/}"; echo $line >> $SORT/log.txt; MALID=${line#*:*:*:*:*:}; VALUES=${line#*:}; TITLE=${VALUES%:*:*:*:*}; FANSUB1=${VALUES%:*:*:*}; FANSUB=${FANSUB1#*:}; NEXT1=${VALUES%:*}; NEXT=${NEXT1#*:*:}; AUDIO=${NEXT%:*}; SUB=${NEXT#*:}
+	# File meta title
+	FTITLE="$MALID|$TITLE|$FANSUB|$AUDIO|$SUB"; echo Filetitle $FTITLE >> $SORT/log.txt
+	# Verify meta colon count
 	colon_count=$(grep -o ":" <<< "$line" | wc -l)
 	if [[ ! $colon_count == 5 ]]; then
-		declare -a arr=($MALID $TITLE $FANSUB $AUDIO $SUB)
-		echo "> invalid colon count! $colon_count" >> $SORT/log.txt
+		declare -a arr=($MALID $TITLE $FANSUB $AUDIO $SUB);	echo "> invalid colon count! $colon_count" >> $SORT/log.txt
 		for i in "${arr[@]}"; do
 			if [[ $i == *":"* ]]; then
 				echo "> $i: [$i]" >> $SORT/log.txt
 			fi
 		done
+		die "invalid colon ${line}" >> $SORT/log.txt
 	else
-		echo "" >> $SORT/log.txt
-		
+		# If file in working directory
 		if [[ $TR_TORRENT_NAME == *".mkv" ]] || [[ $TR_TORRENT_NAME == *".mp4" ]] || [[ $Basename == *".mkv" ]] || [[ $Basename == *".mp4" ]]; then
 			echo "file $FILEN" >> $SORT/log.txt
 			if [[ -f $FILEN ]]; then
-				echo 'match!' >> $SORT/log.txt
-				echo "$FILEN"
-				echo "match!"
-				echo "id["$MALID"] title["$TITLE"] fansub["$FANSUB"] file["$FILEN"]" >> $SORT/log.txt
+				echo "match!"; echo "match!" >> $SORT/log.txt; echo "$FILEN"; echo "id["$MALID"] title["$TITLE"] fansub["$FANSUB"] file["$FILEN"]" >> $SORT/log.txt
 				#mediainfo --fullscan "$FILEN"
 				if [[ $TR_TORRENT_NAME == *".mkv" ]] || [[ $Basename == *".mkv" ]]; then
+					# Remove pipe if exist
 					if [[ $file == *"|"* ]]; then
 						mv "${FILEN}" "${FILEN/|/}"; file=${file//|/};
 					fi
+					# Set file title metadata
 					mkvpropedit "$FILEN" -e info -s title="$FTITLE" >> $SORT/log.txt
+					# Move to downloads folder
 					mv ${SORT}/*.mkv ${DOWNLOAD}
-					/root/bot.sh sort
+					nohup /root/bot.sh sort > /dev/null 2>&1 &
 					die "complete" >> $SORT/log.txt
 				fi
 			else
@@ -65,33 +48,41 @@ while read line; do
 			fi
 		sleep 2
 		else
+			# If file in subdirectory
 			echo "directory" >> $SORT/log.txt
 			echo $Basename >> $SORT/log.txt
+			# Rename variable to remove illegal characters
 			FILEN1=${Basename//[^a-zA-Z_0-9]/_}
 			if [ -d "$FILEN" ]; then
+				# Rename folder to remove illegal characters
 				RENAME=${FILEN1/[^a-zA-Z_0-9]/_}
 				mv "$FILEN" "$SORT/$RENAME"
+				# Change working directory into folder
 				cd "$SORT/$FILEN1"
+				# If moved successfully into folder
 				if [ ! $(pwd) == $SORT ]; then
-				echo "match!"
-				echo "id["$MALID"] title["$TITLE"] fansub["$FANSUB"] file["$FILEN"]" >> $SORT/log.txt
-					for file in *mkv; do
-						echo Current directory $(pwd) >> $SORT/log.txt
-						if [[ $file == *".mkv" ]]; then
-							if [[ $file == *"|"* ]]; then
-								mv "${file}" "${file/|/}"; file=${file//|/};
-							fi
-							mkvpropedit "$file" -e info -s title="$FTITLE" >> $SORT/log.txt
-							mv "${file}" "${DOWNLOAD}" >> $SORT/log.txt
+					echo "match!"; echo "id["$MALID"] title["$TITLE"] fansub["$FANSUB"] file["$FILEN"]" >> $SORT/log.txt
+					# Move files into working directory [Max 1 subfolder]
+					mv ***/*.mkv "$SORT/$FILEN1"; sleep 2
+					echo "Working directory" $(pwd) >> $SORT/log.txt
+					for file in *.mkv; do
+						# Rename file to remove pipe
+						if [[ $file == *"|"* ]]; then
+							mv "${file}" "${file/|/}"; file=${file//|/};
 						fi
+						# Set file title metadata
+						mkvpropedit "$file" -e info -s title="$FTITLE" >> $SORT/log.txt
+						# Move to downloads folder
+						mv "${file}" "${DOWNLOAD}" >> $SORT/log.txt
 					done
 				else
 					echo "working directory failed change > $SORT/$FILEN1" >> $SORT/log.txt
 				fi
 				sleep 2
+				# Move folder into trash/ID
 				mkdir -p "$TRASH/$MALID"
 				mv "$SORT/$FILEN1" "$TRASH/$MALID"
-				/root/bot.sh sort
+				nohup /root/bot.sh sort > /dev/null 2>&1 &
 				die "complete" >> $SORT/log.txt
 			else
 				echo "file $FILEN" >> $SORT/log.txt
