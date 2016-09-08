cb7452a532186865a44fbd012200a9585245c115
Yubikiri <onevoltten@googlemail.com>
2016-09-08 12:18:57 +1000

    bot.sh move downloads files to .queue even if ffmpeg running

--- bot.sh
+++ bot.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
 cd /root
-SOURCE="/var/www/downloads"
-SECOND="/var/www/downloads/.00"
-QUEUE="/var/www/downloads/.queue"
-ENCODED="/var/www/encoded"
-countqueue=`ls -1 $QUEUE/*.{mkv,mp4,ass} 2>/dev/null | wc -l`
-countencoded=`ls -1 $ENCODED/*.mp4 2>/dev/null | wc -l`
-countsource=`ls -1 $SOURCE/*{mkv,mp4,ass} 2>/dev/null | wc -l`
-countsecond=`ls -1 $SECOND/*.{mkv,mp4,avi} 2>/dev/null | wc -l`
+SOURCE="/var/www/downloads"; SECOND="/var/www/downloads/.00"; QUEUE="/var/www/downloads/.queue"; ENCODED="/var/www/encoded"; countqueue=`ls -1 $QUEUE/*.{mkv,mp4,ass} 2>/dev/null | wc -l`
+countencoded=`ls -1 $ENCODED/*.mp4 2>/dev/null | wc -l`; countsource=`ls -1 $SOURCE/*{mkv,mp4,ass} 2>/dev/null | wc -l`; countsecond=`ls -1 $SECOND/*.{mkv,mp4,avi} 2>/dev/null | wc -l`
 
 if [[ -z "$1" && "$1"=="sort" ]]; then
 	echo "execute retieve worker..."
@@ -22,24 +15,24 @@ fi
 
 php /root/renameu.php
 php /root/rename.php downloads
-#php ~/rename.php 00
+sleep 1
 
-if [ $countqueue != 0 ]; then
-	echo "queue not empty..."
-elif [ $countqueue == 0 ]; then
-	if [ $countsource != 0 ]; then
-		echo "execute rename..."
-		mv ${SOURCE}/*.mkv ${QUEUE}; sleep 2
+if [ $countsource != 0 ]; then
+	echo "execute rename..."
+	mv ${SOURCE}/*.mkv ${QUEUE}; sleep 2
+	PROCESS_DT=$(ps -C ffmpeg -o ruser=)
+	if [[ $PROCESS_DT == "ffmpeg" ]]; then
 		echo "execute encode..."
 		nohup /root/encode.sh > /dev/null 2>&1 &
-	#elif [ $countsecond != 0 ]; then
+	else
+		echo "isogashii desu..."
+	fi
+#elif [ $countsecond != 0 ]; then
+	#php ~/rename.php 00
 	#	echo "Adding secondary files to queue..."
 	#	mv $(ls -1tr $SECOND/*.{mkv,mp4,avi} | grep -E '^[^d]' | head -1) ${QUEUE}; sleep 2
 	#	echo "Process the queue..."
 	#	nohup /root/encode.sh > /dev/null 2>&1 &
-	else
-		echo "oyasumi..."
-	fi
 else
-	echo "isogashii desu..."
+	echo "oyasumi..."
 fi
