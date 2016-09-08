d561214e910c29c8d5524d25caa48e10fa0c3388
Yubikiri <onevoltten@googlemail.com>
2016-09-08 21:32:28 +1000

    bot.sh fixed multiple ffmpeg (v2)

--- bot.sh
+++ bot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /root
+
 SOURCE="/var/www/downloads"; SECOND="/var/www/downloads/.00"; QUEUE="/var/www/downloads/.queue"; ENCODED="/var/www/encoded"; countqueue=`ls -1 $QUEUE/*.{mkv,mp4,ass} 2>/dev/null | wc -l`
 countencoded=`ls -1 $ENCODED/*.mp4 2>/dev/null | wc -l`; countsource=`ls -1 $SOURCE/*{mkv,mp4,ass} 2>/dev/null | wc -l`; countsecond=`ls -1 $SECOND/*.{mkv,mp4,avi} 2>/dev/null | wc -l`
 
@@ -14,18 +15,16 @@ php /root/rename.php
 php /root/rename.php downloads
 sleep 1
 
-PROCESS_DT=$(ps -C ffmpeg -o ruser=)
-if [[ $countsource != 0 && ! $PROCESS_DT == "ffmpeg" ]]; then
+if [[ $countsource != 0 ]]; then
 	if [ $countqueue == 0 ]; then
 		echo "execute rename..."; mv ${SOURCE}/*.mkv ${QUEUE}; sleep 1
-		if [[ ! $PROCESS_DT == "ffmpeg" ]]; then
-			echo "execute encode..."; nohup /root/encode.sh > /dev/null 2>&1 &
-			#sleep 10; If count ffmpeg > 1 kill
+		if pidof -s ffmpeg > /dev/null; then
+			echo 'ffmpeg running...'
 		else
-			echo "nope."
+			echo "execute encode..."; nohup /root/encode.sh > /dev/null 2>&1 &
 		fi
 	else
-		echo "nope..."
+		echo "queue contain files..."
 	fi
 else
 	echo "isogashii desu..."
