77cc04087a0e60b3e2d38c63cc2a3c0377f40aab
Yubikiri <onevoltten@googlemail.com>
2016-09-09 16:39:44 +1000

    bpt.sh revised workflow, improved log

--- bot.sh
+++ bot.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 cd /root
-
 SOURCE="/var/www/downloads"; SECOND="/var/www/downloads/.00"; QUEUE="/var/www/downloads/.queue"; ENCODED="/var/www/encoded"; countqueue=`ls -1 $QUEUE/*.{mkv,mp4,ass} 2>/dev/null | wc -l`
-countencoded=`ls -1 $ENCODED/*.mp4 2>/dev/null | wc -l`; countsource=`ls -1 $SOURCE/*{mkv,mp4,ass} 2>/dev/null | wc -l`; countsecond=`ls -1 $SECOND/*.{mkv,mp4,avi} 2>/dev/null | wc -l`
+countencoded=`ls -1 $ENCODED/*.mp4 2>/dev/null | wc -l`; countsource=`ls -1 $SOURCE/*{mkv,mp4,ass} 2>/dev/null | wc -l`; countsecond=`ls -1 $SECOND/*.{mkv,mp4,avi} 2>/dev/null | wc -l`; lastlog=`tail -1 /root/log.txt | head -1`;
 
 if [[ -z "$1" && "$1"=="sort" ]]; then
-	echo "execute retieve worker..."; nohup /root/retrieve.sh keep &
+	echo "execute retieve worker..."; nohup /root/retrieve.sh keep >/dev/null 2>&1 &
 fi
 if [ $countencoded != 0 ]; then
-	echo "execute upload worker..."; nohup php /root/NodefilesUploader.php &
+	echo "execute upload worker..." >> /root/log.txt; nohup php /root/NodefilesUploader.php >> /root/log.txt &
 fi
 
-php /root/rename.php
-php /root/rename.php downloads
-sleep 1
+php /root/rename.php downloads  >> /root/log.txt; sleep 1
 
 if [[ $countsource != 0 ]]; then
 	if [ $countqueue == 0 ]; then
-		echo "execute rename..."; mv ${SOURCE}/*.mkv ${QUEUE}; sleep 1
+		echo "execute rename..." >> /root/log.txt; mv ${SOURCE}/*.mkv ${QUEUE}; sleep 1
 		if pidof -s ffmpeg > /dev/null; then
-			echo 'ffmpeg running...'
+			if [[ ! $lastlog == "ffmpeg running..." ]]; then
+				echo "ffmpeg running..." >> /root/log.txt
+			fi
 		else
-			echo "execute encode..."; nohup /root/encode.sh &
+			echo "execute encode..." >> /root/log.txt; gnome-terminal -e /root/encode.sh >> /root/log.txt &
 		fi
 	else
-		echo "queue contain files..."
+		if [[ ! $lastlog == "queue contain files..." ]]; then
+			echo "queue contain files..." >> /root/log.txt
+		fi
 	fi
 elif pidof -s ffmpeg > /dev/null; then
-	echo 'ffmpeg running...'
+	if [[ ! $lastlog == "ffmpeg running..." ]]; then
+		echo "ffmpeg running..." >> /root/log.txt
+	fi
+elif [[ $countqueue != 0 ]]; then
+	sleep 15
+	if pidof -s ffmpeg > /dev/null; then
+		echo "ffmpeg running..."
+	else
+		if [[ ! $lastlog == "queue contain files..." ]]; then
+			echo "queue contain files..." >> /root/log.txt
+		fi
+	fi
 else
-	echo "sabishī yo..."
+	echo "sabishī yo..." >> /root/log.txt
 fi
