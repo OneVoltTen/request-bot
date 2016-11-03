52886c65989da33028efc3535fadbda3247bf538
Liam B <onevoltten@googlemail.com>
2016-11-03 12:11:12 +0900

    Reconfigure for localhost

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ var cheerio = require('cheerio');
 var Transmission = require('transmission');
 var transmission = new Transmission();
 var count=0;
-var url = 'https://beta.animepahe.com/devrequest/0';
+var url = 'http://localhost/request.php';
+// var url = 'https://beta.animepahe.com/devrequest/0';
 // https://www.nyaa.se/?page=view&tid=607865
 // https://bakabt.me/torrent/148508/07-ghost-720p-hatsuyuki
 
--- app/config.php
+++ app/config.php
@@ -1,5 +1,6 @@
 <?php
 date_default_timezone_set("UTC");
+define('DB_HOST','209.58.180.26');
 define('DB_NAME', 'animepahe');
 define('DB_USER', 'root');
 define('DB_PASS', 'y@nGsu5ah2o16');
@@ -8,6 +9,5 @@ define('GWSHARE_PASS', 'Anime');
 define('ENCODED', '/var/www/encoded');
 define('UPLOADED', '/var/www/uploaded');
 define('KOMARU', '/var/www/komaru');
-define('DB_HOST','209.58.180.26');
 define('GROUP','AnimePahe');
 ?>
