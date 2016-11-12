9cb5e02ce57f0b71ed9250770d92454f383f81b4
Liam B <onevoltten@googlemail.com>
2016-11-12 10:50:12 +1000

    moved log message to config.sh

--- app/config.sh
+++ app/config.sh
@@ -10,6 +10,16 @@ VERIFY="$WWW/verify"
 UPLOAD="$WWW/upload"
 UPLOADED="$INSTALL/uploaded"
 
+# log message
+ERW="execute retieve worker..."
+QCF="queue contain files..."
+ER="execute rename..."
+FR="ffmpeg running..."
+PR="php running..."
+EE="execute encode..."
+FR="ffmpeg running..."
+SY="sabishī yo..."
+
 TRASH="$WWW/trash"
 LOG="$INSTALL/log"
 GROUP="AnimePahe"
@@ -17,4 +27,12 @@ GROUP="AnimePahe"
 lastlog=`tail -1 "${LOG}/main.log" | head -1`
 
 die() { echo "$@" 1>&2 ; exit 1; }
+
+#arrayr=("ffmpeg" "transmission-daemon" "php" "node" "mkvmerge")
+#for i in "${arrayr[@]}"; do
+#    $i -v foo >/dev/null 2>&1 || {
+#        die "$i komaru!";
+#    }
+#done
+
 cd ${INSTALL}
--- bot.sh
+++ bot.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 . /root/app/config.sh
 
-#revise log comments
-ERW="execute retieve worker..."
-QCF="queue contain files..."
-ER="execute rename..."
-FR="ffmpeg running..."
-PR="php running..."
-EE="execute encode..."
-FR="ffmpeg running..."
-SY="sabishī yo..."
-
 nohup ${INSTALL}/app/retrieve.sh >> ${LOG}/retrieve.log &
-php ${INSTALL}/upload/upload.php >> ${LOG}/upload.log &
-php ${INSTALL}/rename/rename.php downloads >> ${LOG}/rename.log &
-#nohup ${INSTALL}/encode/encode.sh >> ${LOG}/main.log &
+nohup php ${INSTALL}/upload/upload.php >> ${LOG}/upload.log &
+nohup php ${INSTALL}/rename/rename.php downloads >> ${LOG}/rename.log &
+nohup ${INSTALL}/encode/encode.sh >> ${LOG}/main.log &
