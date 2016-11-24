fa1dd891700f21cbcee47e273664e41566e8d746
Liam B <onevoltten@googlemail.com>
2016-11-25 09:39:37 +1000

    revision

--- bot.sh
+++ bot.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 . /root/app/config.sh
 
-nohup ${INSTALL}/app/retrieve.sh >> ${LOG}/retrieve.log &
+nohup ${INSTALL}/app/retrieve.sh >> ${LOG}/retrieve.log & # retrieve torrent list and initiate torrents
+nohup php /root/rename/rename.php downloads >> ${LOG}/rename.log & # rename sorted files into queue folder
+nohup ${INSTALL}/encode/encode.sh >> ${LOG}/main.log & # encode files in queue folder to upload folder
 
-echo ps -ef | grep "upload.php" | grep -v grep
+echo ps -ef | grep "upload.php" | grep -v grep # execute upload.php if not running
 if [[ $? -eq 0 ]]; then
     echo "upload"
     nohup php ${INSTALL}/upload/upload.php >> ${LOG}/upload.log &
 fi
-
-nohup php ${INSTALL}/rename/rename.php downloads >> ${LOG}/rename.log &
-nohup ${INSTALL}/encode/encode.sh >> ${LOG}/main.log &
