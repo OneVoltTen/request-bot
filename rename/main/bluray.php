74c01d4d252928cc294994c4dd78b6fbd80eb0ae
Liam B <onevoltten@googlemail.com>
2016-11-27 12:17:11 +1000

    fixed syntax error

--- rename/main/bluray.php
+++ rename/main/bluray.php
@@ -2,7 +2,7 @@
 	// Check BD/DVD
 	if (strpos($str,'_BD')==true || stripos($str,'bluray')==true || stripos($str,'blu-ray')==true || stripos($str,'bdrip')==true || stripos($str,'brip')==true || stripos($str,'brrip')==true || stripos($str,'bray')==true){
 	$disc="BD";
-	$str=str_ireplace(array("bray","brrip","brip","bdrip","blu-ray","bluray","blu_ray","BD","dvd","dvdrip"),$str);
+	$str=str_ireplace(array("bray","brrip","brip","bdrip","blu-ray","bluray","blu_ray","BD","dvd","dvdrip"),'',$str);
 	}elseif(strpos($str,'DVD')==true){
 		$disc="DVD";
 	}
