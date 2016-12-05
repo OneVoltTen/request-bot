7209720a3009165d49ea41927d91cb41a5ed6d6f
OneVoltTen <onevoltten@googlemail.com>
2016-12-05 16:33:18 +0000

    update

--- app/config.php
+++ app/config.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set("UTC");
 
-define('DB_HOST','209.58.180.26');
+define('DB_HOST','77.73.64.74');
 define('DB_NAME', 'animepahe');
-define('DB_USER', 'root');
+define('DB_USER', 'root2');
 define('DB_PASS', 'y@nGsu5ah2o16');
 
 define('GWSHARE_USER', 'Anime');
