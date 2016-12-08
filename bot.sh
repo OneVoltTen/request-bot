fa5ea526402f7782b9b4bfec5fc0e37b533a290f
Liam B <onevoltten@googlemail.com>
2016-12-08 16:21:55 +1000

    Fixed lock logic on arch linux

--- bot.sh
+++ bot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 . /root/app/config.sh
 
-LOCKFILE=/tmp/lockbot.txt # create lockfile
+LOCKFILE="/tmp/lock.txt" # create lockfile
 if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
     echo "bot.sh already running" >> ${LOG}/main.log
     exit
 else
-	echo $$ > ${LOCKFILE} # set process id into lockfile
-	trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+    echo $$ > ${LOCKFILE} # set process id into lockfile
+    trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
 fi
 
 nohup ${INSTALL}/sort/sort.sh >> ${LOG}/sort.log  &
@@ -15,12 +15,10 @@ nohup ${INSTALL}/app/retrieve.sh >> ${LOG}/retrieve.log &
 nohup php /root/rename/rename.php downloads >> ${LOG}/rename.log &
 nohup ${INSTALL}/encode/encode.sh >> ${LOG}/main.log &
 
-echo ps -ef | grep "upload.php" | grep -v grep
-if [[ $? -eq 0 ]]; then
-    echo "upload"
-    nohup php ${INSTALL}/upload/upload.php >> ${LOG}/upload.log &
+if ps -ef | grep -v grep | grep upload.php >> ${LOG}/main.log ; then
+    exit 0
+else
+    nohup php ${INSTALL}/upload/upload.php >> ${LOG}/upload.log &   
 fi
-
-sleep 30
 
 rm -f ${LOCKFILE}
